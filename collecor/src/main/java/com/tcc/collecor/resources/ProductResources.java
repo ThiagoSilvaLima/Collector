7f617f0927c80cf75000bf56446ccdc5024d9493
Thiago da Silva Lima <118988854+ThiagoSilvaLima@users.noreply.github.com>
2023-09-04 10:40:20 -0300

    pequenas mudanças

--- collecor/src/main/java/com/tcc/collecor/resources/ProductResources.java
+++ collecor/src/main/java/com/tcc/collecor/resources/ProductResources.java
@@ -15,7 +15,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
-import org.springframework.web.servlet.ModelAndView;
 
 import com.tcc.collecor.entities.Product;
 import com.tcc.collecor.services.ProductService;
