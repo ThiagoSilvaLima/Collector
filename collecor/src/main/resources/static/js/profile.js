4caabd41c27b1c0a2bc3d7216850472071ce4975
Lívia Sartori <129086143+NyYurian@users.noreply.github.com>
2023-11-14 13:20:17 -0200

    IconPerfil

---
+++ collecor/src/main/resources/static/js/profile.js
@@ -0,0 +1,246 @@
+const path = document.getElementById('paths');
+const img = document.getElementById('animeAstro');
+const img2 = document.getElementById('caveira');
+const img3 = document.getElementById('zumbi');
+const img4 = document.getElementById('animeAstro2');
+const img5 = document.getElementById('menina_caveira');
+const img6 = document.getElementById('astroFofo');
+const img7 = document.getElementById('astroFofo2');
+const img8 = document.getElementById('astroFofa');
+const img9 = document.getElementById('astroFofa2');
+const img10 = document.getElementById('rino_1');
+const img11 = document.getElementById('rino_2');
+const img12 = document.getElementById('zumbi2');
+
+function whithoutborder(){
+    img.style.borderStyle = "none"
+    img2.style.borderStyle = "none"
+    img3.style.borderStyle = "none"
+    img4.style.borderStyle = "none"
+    img5.style.borderStyle = "none"
+    img6.style.borderStyle = "none"
+    img7.style.borderStyle = "none"
+    img8.style.borderStyle = "none"
+    img9.style.borderStyle = "none"
+    img10.style.borderStyle = "none"
+    img11.style.borderStyle = "none"
+    img12.style.borderStyle = "none"
+}
+
+
+function selectImage() {
+    if ((img.style.borderStyle = "solid") || (img2.style.borderStyle = "solid") || (img.style.borderStyle = "solid")) {
+        whithoutborder();
+
+        path.value = "/imgs/profileIcons/animeAstro.jpg";
+
+        img.style.borderStyle = "solid";
+        img.style.borderWidth = "3px";
+        img.style.borderColor = "red";
+    } else {
+        img.style.borderStyle = "solid";
+        img.style.borderWidth = "3px";
+        img.style.borderColor = "red";
+    }
+    
+}
+
+function selectImage2() {
+    if ((img.style.borderStyle = "solid") || (img2.style.borderStyle = "solid") || (img.style.borderStyle = "solid")) {
+        whithoutborder();
+
+        path.value = "/imgs/profileIcons/caveira.jpg";
+
+        img2.style.borderStyle = "solid";
+        img2.style.borderWidth = "3px";
+        img2.style.borderColor = "red";
+    } else {
+        img2.style.borderStyle = "solid";
+        img2.style.borderWidth = "3px";
+        img2.style.borderColor = "red";
+    }
+
+}
+
+function selectImage3() {
+    if ((img.style.borderStyle = "solid") || (img2.style.borderStyle = "solid") || (img.style.borderStyle = "solid")) {
+        whithoutborder();
+
+        path.value = "/imgs/profileIcons/zumbi.jpg";
+
+        img3.style.borderStyle = "solid";
+        img3.style.borderWidth = "3px";
+        img3.style.borderColor = "red";
+    } else {
+        img3.style.borderStyle = "solid";
+        img3.style.borderWidth = "3px";
+        img3.style.borderColor = "red";
+    }
+    
+}
+function selectImage4() {  
+
+    if ((img.style.borderStyle = "solid") || (img2.style.borderStyle = "solid") || (img.style.borderStyle = "solid")) {
+        whithoutborder();
+
+        path.value = "/imgs/profileIcons/animeAstro2.jpg";
+
+        img4.style.borderStyle = "solid";
+        img4.style.borderWidth = "3px";
+        img4.style.borderColor = "red";
+    } else {
+        img4.style.borderStyle = "solid";
+        img4.style.borderWidth = "3px";
+        img4.style.borderColor = "red";
+    }
+    
+}
+function selectImage5() {
+    if ((img.style.borderStyle = "solid") || (img2.style.borderStyle = "solid") || (img.style.borderStyle = "solid")) {
+        whithoutborder();
+        path.value = "/imgs/profileIcons/menina_caveira.jpg";
+
+        img5.style.borderStyle = "solid";
+        img5.style.borderWidth = "3px";
+        img5.style.borderColor = "red";
+    } else {
+        img5.style.borderStyle = "solid";
+        img5.style.borderWidth = "3px";
+        img5.style.borderColor = "red";
+    }
+}    
+function selectImage6() {
+    if ((img.style.borderStyle = "solid") || (img2.style.borderStyle = "solid") || (img.style.borderStyle = "solid")) {
+        whithoutborder();
+
+        path.value = "/imgs/profileIcons/astroFofo.jpg";
+
+        img6.style.borderStyle = "solid";
+        img6.style.borderWidth = "3px";
+        img6.style.borderColor = "red";
+    } else {
+        img6.style.borderStyle = "solid";
+        img6.style.borderWidth = "3px";
+        img6.style.borderColor = "red";
+    }
+}
+function selectImage7() {
+
+    
+
+    if ((img.style.borderStyle = "solid") || (img2.style.borderStyle = "solid") || (img.style.borderStyle = "solid")) {
+        whithoutborder();
+
+        path.value = "/imgs/profileIcons/astroFofo2.jpg";
+
+        img7.style.borderStyle = "solid";
+        img7.style.borderWidth = "3px";
+        img7.style.borderColor = "red";
+    } else {
+        img7.style.borderStyle = "solid";
+        img7.style.borderWidth = "3px";
+        img7.style.borderColor = "red";
+    }
+}
+function selectImage8() {
+    if ((img.style.borderStyle = "solid") || (img2.style.borderStyle = "solid") || (img.style.borderStyle = "solid")) {
+        whithoutborder();
+
+        path.value = "/imgs/profileIcons/astroFofa.jpg";
+
+        img8.style.borderStyle = "solid";
+        img8.style.borderWidth = "3px";
+        img8.style.borderColor = "red";
+    } else {
+        img8.style.borderStyle = "solid";
+        img8.style.borderWidth = "3px";
+        img8.style.borderColor = "red";
+    }
+    
+
+
+}
+function selectImage9() {
+
+    
+
+    if ((img.style.borderStyle = "solid") || (img2.style.borderStyle = "solid") || (img.style.borderStyle = "solid")) {
+        whithoutborder();
+
+        path.value = "/imgs/profileIcons/astroFofa2.jpg";
+
+        img9.style.borderStyle = "solid";
+        img9.style.borderWidth = "3px";
+        img9.style.borderColor = "red";
+    } else {
+        img9.style.borderStyle = "solid";
+        img9.style.borderWidth = "3px";
+        img9.style.borderColor = "red";
+    }
+    
+
+
+}
+function selectImage10() {
+
+    
+
+    if ((img.style.borderStyle = "solid") || (img2.style.borderStyle = "solid") || (img.style.borderStyle = "solid")) {
+        whithoutborder();
+
+        path.value = "/imgs/profileIcons/rino_1.jpg";
+
+        img10.style.borderStyle = "solid";
+        img10.style.borderWidth = "3px";
+        img10.style.borderColor = "red";
+    } else {
+        img10.style.borderStyle = "solid";
+        img10.style.borderWidth = "3px";
+        img10.style.borderColor = "red";
+    }
+    
+
+
+}
+function selectImage11() {
+
+    
+
+    if ((img.style.borderStyle = "solid") || (img2.style.borderStyle = "solid") || (img.style.borderStyle = "solid")) {
+        whithoutborder();
+
+        path.value = "/imgs/profileIcons/rino_2.jpg";
+
+        img11.style.borderStyle = "solid";
+        img11.style.borderWidth = "3px";
+        img11.style.borderColor = "red";
+    } else {
+        img11.style.borderStyle = "solid";
+        img11.style.borderWidth = "3px";
+        img11.style.borderColor = "red";
+    }
+    
+
+
+}
+function selectImage12() {
+
+    
+
+    if ((img.style.borderStyle = "solid") || (img2.style.borderStyle = "solid") || (img.style.borderStyle = "solid")) {
+        whithoutborder();
+
+        path.value = "/imgs/profileIcons/zumbi2.jpg";
+
+        img12.style.borderStyle = "solid";
+        img12.style.borderWidth = "3px";
+        img12.style.borderColor = "red";
+    } else {
+        img12.style.borderStyle = "solid";
+        img12.style.borderWidth = "3px";
+        img12.style.borderColor = "red";
+    }
+    
+
+
+}
