4a1c1067cef6ba6cebd99cc540b72ab353a56d3a
Thiago da Silva Lima <118988854+ThiagoSilvaLima@users.noreply.github.com>
2023-11-26 17:41:09 -0300

    visual adjusts

--- collecor/src/main/resources/static/js/loginpopup.js
+++ collecor/src/main/resources/static/js/loginpopup.js
@@ -3,12 +3,15 @@ var modal = document.getElementById("loginModal");
 
 // Get the button that opens the modal
 var btn = document.getElementById("btnLogin");
+var redirLogin = document.getElementById("redirLogin");
+
 
 // Get the modal
-var siginModal = document.getElementById("siginModal");
+var signModal = document.getElementById("signModal");
 
 // Get the button that opens the modal
 var btnSign = document.getElementById("btnSign");
+var redirSign = document.getElementById("redirSign");
 
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
@@ -19,15 +22,24 @@ var spanS = document.getElementsByClassName("close2")[0];
 btn.onclick = function() {
     modal.style.display = "block";
 }
-btnSign.onclick = function() {
-    siginModal.style.display = "block";
+redirLogin.onclick = function(){
+    modal.style.display = "block";
+    signModal.style.display = "none";
 }
+btnSign.onclick = function() {
+    signModal.style.display = "block";
+}
+redirSign.onclick=function(){
+    signModal.style.display = "block";
+    modal.style.display = "none";
+}
+
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() {
     modal.style.display = "none";       
 }
 spanS.onclick = function() {
-    siginModal.style.display = "none";       
+    signModal.style.display = "none";       
 }
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
@@ -36,7 +48,7 @@ window.onclick = function(event) {
     }
 }
 window.onclick = function(event) {
-if (event.target == siginModal) {
-    siginModal.style.display = "none";
+if (event.target == signModal) {
+    signModal.style.display = "none";
 }
 }
--- collecor/src/main/resources/static/js/loja.js
+++ collecor/src/main/resources/static/js/loja.js
@@ -8,3 +8,11 @@ function favoriteSuccess(){
     var element = document.getElementById("result");
     element.style.visibility="hidden";
 }
+
+function hideDescription(){
+    var element = document.getElementById("btDesc");
+    if(element.innerHTML=="Ocultar descrição"){
+        element.innerHTML="Exibir descrição";
+    }else
+    element.innerHTML="Ocultar descrição";
+}
